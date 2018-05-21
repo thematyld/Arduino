81192d54f605c2b7ba5816be6de79087075b1a94
Martin Bartos <thematyld@gmail.com>
2018-05-21 23:13:34 +0200

    Added IR

--- MembraneKey/bejvak/bejvak.ino
+++ MembraneKey/bejvak/bejvak.ino
@@ -1,10 +1,19 @@
+#include "IRremote.h"
 #define KEY_COUNT 2
+
+//-----------------------------------------------------------------------------//
 const int rele[4]={8,9,10,11};
 const int keys[4]={2,3,4,5};
+const int IR=12;
+IRrecv irrecv(IR);
+decode_results IRresult;
 int key[4];
 char keyOn[4];
 unsigned long prevTime[4];
-
+//-----------------------------------------------------------------------------//
+void decodeIR();
+void turnKey(byte numKey,unsigned int minTime,int device,unsigned long *lastTime);
+//-----------------------------------------------------------------------------//
 void setup() {
   for(int i=0;i<KEY_COUNT;i++){
     keyOn[i]=false;
@@ -13,15 +22,42 @@ void setup() {
     digitalWrite(rele[i],HIGH);
     pinMode(keys[i],INPUT_PULLUP);  
   }
+  irrecv.enableIRIn();
   Serial.begin(9600);
 }
 
 void loop() {
   turnKey(0,750,rele[0],&prevTime[0]);
   turnKey(1,750,rele[1],&prevTime[1]);
+  if(irrecv.decode(&IRresult)){
+    decodeIR();
+    irrecv.resume();
+    if(keyOn[0]==true)
+      Serial.println("AAA");
+    else
+      Serial.println("NNN");
+  }
   delay(10);
 }
 
+void decodeIR(){
+  switch(IRresult.value){
+    case 0xFF6897: IRturning(rele[0],&keyOn[0]);    break;
+    case 0xFF9867: Serial.println(" 2");    break;
+    } 
+  }
+  
+void IRturning(int device,char *state){
+  if(state){
+    digitalWrite(device,LOW);
+    *state=false;  
+  }
+  else{
+    digitalWrite(device,HIGH);
+    *state=true;
+    }
+  }
+    
 void turnKey(byte numKey,unsigned int minTime,int device,unsigned long *lastTime){
   if(numKey>=KEY_COUNT)
     return;
